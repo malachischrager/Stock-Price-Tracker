029204a5662b995a6f5b747cc13f8a392a8db69e
malachischrager <malachischrager@Malachis-MacBook-Pro.local>
2021-02-19 11:08:29 -0800

    added mocha

---
+++ test/pointsWithProfit.test.js
@@ -0,0 +1,11 @@
+// npm run test or npm run test test/pointwithProfit.test.js
+
+const assert = require('assert');
+
+describe('proft value true', () => {
+    it('should return true when profit value > 0', () => {
+        assert.equal(true, true);
+    });
+
+});
+
