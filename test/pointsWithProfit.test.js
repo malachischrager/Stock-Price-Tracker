45c5a00162d18e3a17a10df3326ebb1db5cd07aa
malachischrager <malachischrager@Malachis-MacBook-Pro.local>
2021-02-26 09:36:11 -0800

    testpointsWIthProfit

--- test/pointsWithProfit.test.js
+++ test/pointsWithProfit.test.js
@@ -1,10 +1,58 @@
 // npm run test or npm run test test/pointwithProfit.test.js
 
-const assert = require('assert');
+var expect = require("chai").expect;
+const {pointsWithProfit} = require('../functions');
 
-describe('proft value true', () => {
-    it('should return true when profit value > 0', () => {
-        assert.equal(true, true);
+describe('only one ohlc and rsi', () => {
+    it('should return empty array', () => {
+        
+        var result = pointsWithProfit(
+            {
+                '2021-02-25 09:30:00': {
+                    '1. open': '166.0000',
+                    '2. high': '170.0000',
+                    '3. low': '165.0000',
+                    '4. close': '169.3600',
+                    '5. volume': '131754'
+                  }
+
+            }
+            ,
+
+            {
+                '2021-02-25 09:30': { RSI: '51.1293' }
+            }
+
+            , 0.03, 20, 50
+        )
+
+        console.log("HIHIHI" + result);
+
+        expect(result).to.deep.equal([
+            
+        ]);
+    });
+
+});
+
+describe('when invalid paramters passed in', () => {
+    it('should return empty array', () => {
+        
+        var result = pointsWithProfit(
+            {
+
+            }
+            ,
+            {
+                
+            }
+
+            , 0, 0, 0
+        )
+
+        console.log("HIHIHI" + result);
+
+        expect(result).to.eql({});
     });
 
 });
